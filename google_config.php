a68d84198111de8bf92ef70472f0b38c5807194f
ajit <ajitgupta9639@gmail.com>
2022-09-09 12:19:17 +0530

    done

--- google_config.php
+++ google_config.php
@@ -7,9 +7,9 @@ define('DB_NAME', 'login_with_google');
 define('DB_USER_TBL', 'users');
 
 // Google API configuration
-define('GOOGLE_CLIENT_ID', '476051899607-u15aomq2qsiadu6i842ejsjng2ln50au.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URL', 'http://localhost/PHP-Login-With-Google/index.php');
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
+define('GOOGLE_REDIRECT_URL', '');
 
 // Start session
 if (!session_id()) {
@@ -22,9 +22,9 @@ require_once 'google-api-php-client/contrib/Google_Oauth2Service.php';
 
 // Call Google API
 $gClient = new Google_Client();
-$gClient->setApplicationName('Login to www.carry0987.com');
-$gClient->setClientId('476051899607-u15aomq2qsiadu6i842ejsjng2ln50au.apps.googleusercontent.com');
-$gClient->setClientSecret('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');
-$gClient->setRedirectUri('http://localhost/PHP-Login-With-Google/index.php');
+$gClient->setApplicationName('project name');
+$gClient->setClientId('');
+$gClient->setClientSecret('');
+$gClient->setRedirectUri('');
 
 $google_oauthV2 = new Google_Oauth2Service($gClient);
